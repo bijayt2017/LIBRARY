c8152999e2b0dffcda85dd3ca38ba196ccb45f02
bijayt2017 <58078690+bijayt2017@users.noreply.github.com>
2019-12-04 10:18:57 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
-#include <iostream>
-
+ #include <iostream>
+#include <time.h>
+#include <stdlib.h>
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
-}
+    srand(time(NULL));
+    int choice, number;
+    number = (rand() % 100+1);
+
+    do {
+        cout << "Try to guess the Random Number" << endl;
+        cin >> choice ;
+       if (choice>number) {cout << "Too big !"<<endl;}
+       else if (choice<number) {cout << "Too small !" << endl;}
+       else if (choice==number) {
+        cout << "Great you find the number!"<< endl;
+        break;}
+       } while (true);
+ }
+
+
+
